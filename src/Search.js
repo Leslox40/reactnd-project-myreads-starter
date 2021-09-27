6d1204a439a2a49341720aa859879a5d362fe1e7
Leslox40 <fonepiles40x@gmail.com>
2021-09-27 22:10:25 +0200

    Display books from API

--- src/Search.js
+++ src/Search.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import ShelfBookList from './ShelfBookList'
+import * as BooksAPI from './BooksAPI'
 
 class Search extends Component {
 
+    state = {
+        query: '',
+        books: [],
+    };
+
+    handleChange = (event) => {
+        this.setState({ query: event.target.value })
+    }
+
+    updateBooks = (books) => {
+        console.log('books', books)
+        if(books !== undefined ) {
+            this.setState(currentState => ({
+                books,
+            }));
+        }
+        
+    };
+
+    updateUI = () => {
+
+    }
+
     render() {
+        const { query, books } = this.state;
+
+        const showingBooks = query === '' ? 
+        '' :
+        BooksAPI.search(query)
+        .then(booksResult => {
+            this.updateBooks(booksResult);
+        });
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link to='/'
-                        className="close-search" >Close</Link>
+                        className="close-search"
+                    >
+                        Close
+                    </Link>
+
                     <div className="search-books-input-wrapper">
-                        
-                        <input type="text" placeholder="Search by title or author" />
+
+                        <input type="text"
+                            placeholder="Search by title or author"
+                            value={this.state.query}
+                            onChange={this.handleChange}
+                        />
 
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ShelfBookList shelfBooks={books} updateUI={this.updateUI}/>
                 </div>
             </div>
         )
