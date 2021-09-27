648807837e9c774103c213892128b59c678f59b3
Leslox40 <fonepiles40x@gmail.com>
2021-09-27 02:13:59 +0200

    Filter contacts with shelf property of wantToRead

--- src/WantToRead.js
+++ src/WantToRead.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react'
 import BookList from './BookList'
 
 class wantToRead extends Component {
+    state = {
+        wantToReadBooks: [],
+    }
 
+    // Function filter's Books array passed as props and updates state
+    filterWantToReadBooks = () => {
+        const  { books } = this.props;
+        const wantToReadBooks = books.filter((book) => (
+         book.shelf === 'currentlyReading'
+        ));
+        this.setState((currentState) => ({
+            wantToReadBooks
+        }))
+     }
 
     render() {
+       const { wantToReadBooks } = this.state
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">Want to Read</h2>
                 <div className="bookshelf-books">
-                    <BookList />
+                    <BookList wantToRead={wantToReadBooks}/>
                 </div>
             </div>
         )
